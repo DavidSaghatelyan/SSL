27e094913bf70dd7455b0fc137e460dcec4dfee5
DavidSaghatelyan <dsaghatelyanmatos@frba.utn.edu.ar>
2026-04-24 21:27:39 -0300

    hello world con version de C incluida

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int version = __STDC_VERSION__ ;
+    printf("Hello world, estoy usando la version %d de C", version );
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+compilador: gcc
+version de compilador: 15.1.0
+version que el compilador compila: C23 como estándar predeterminado
